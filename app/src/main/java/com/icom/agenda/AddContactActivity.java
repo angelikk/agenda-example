9ed29f164dd467c52aa05ff53bc2986da2b7838a
angelicazavala <angelica.zavala@icomsys.com.mx>
2016-09-19 00:50:44 -0500

    message

--- app/src/main/java/com/icom/agenda/AddContactActivity.java
+++ app/src/main/java/com/icom/agenda/AddContactActivity.java
@@ -1,10 +1,14 @@
 package com.icom.agenda;
 
+import android.media.MediaCodec;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class AddContactActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,17 +28,50 @@ public class AddContactActivity extends AppCompatActivity implements View.OnClic
         etPhone = (EditText) findViewById(R.id.et_phone);
         etEmail = (EditText) findViewById(R.id.et_email);
         etAddress = (EditText) findViewById(R.id.et_address);
+
+        ivAddContactDone.setOnClickListener(this);
     }
 
-    private void validate() {
+    private boolean validate() {
 
+        boolean flag = false;
 
+        if (etName.getText().toString().length() > 0) {
+            if (etPhone.getText().toString().length() > 0) {
+                String regex = "^[0-9]+$";
+                if (etPhone.getText().toString().matches(regex)) {
+                    if (etEmail.getText().toString().matches("^[_A-Za-z0-9-\\\\+]+(\\\\.[_A-Za-z0-9-]+)*\n" +" @[A-Za-z0-9-]+(\\\\.[A-Za-z0-9]+)*(\\\\.[A-Za-z]{2,})$")) {
+                        if (etAddress.getText().toString().length() > 0) {
+                            flag = true;
+                        } else {
+                            flag = false;
+                            Toast.makeText(AddContactActivity.this, "Ingresa una dirección.", Toast.LENGTH_SHORT).show();
+                        }
+                    } else {
+                        flag = false;
+                        Toast.makeText(AddContactActivity.this, "Correo no válido", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    flag = false;
+                    Toast.makeText(AddContactActivity.this, "El número no es válidp", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                flag = false;
+                Toast.makeText(AddContactActivity.this, "Ingresa un número de teléfono", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            flag = false;
+            Toast.makeText(AddContactActivity.this, "Nombre inválido", Toast.LENGTH_SHORT).show();
+        }
+        return flag;
     }
 
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.iv_addContact_done) {
-            validate();
+            if(validate()){
+                Toast.makeText(AddContactActivity.this, "Contacto agregado", Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
